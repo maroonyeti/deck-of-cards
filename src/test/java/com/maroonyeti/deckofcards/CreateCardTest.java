05b003b780028310146f871c275725f9d4b18cc6
Haeyoung Lim <haeyounglim@gmail.com>
2020-01-24 21:08:19 +0000

    Setting up tests

---
+++ src/test/java/com/maroonyeti/deckofcards/CreateCardTest.java
@@ -0,0 +1,18 @@
+package com.maroonyeti.deckofcards;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CreateCardTest {
+
+  @Test
+  public void createCard_RankIsTwo_SuitIsSpade_TwoOfSpadeIsCreated() {
+    fail();
+  }
+
+  @Test
+  public void createCard_RankIsNull_SuitIsNull_JokerIsCreated() {
+    fail();
+  }
+
+}
